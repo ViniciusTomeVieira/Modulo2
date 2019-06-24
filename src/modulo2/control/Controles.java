c5c6b84d9f7dd5f1b295e8208cbd644cad9f142e
Adroan <adroanheinen@gmail.com>
2019-06-24 15:19:46 -0300

    vomitsss

--- src/modulo2/control/Controles.java
+++ src/modulo2/control/Controles.java
@@ -37,66 +37,72 @@ import principal.GeradorXML;
  * @author vinny
  */
 public class Controles {
+
     private Osm osm;
     private ConversorES converterES;
     private Data data;
+    private String xml;
+
     public void geraObjeto(File file) {
         converterES = new ConversorES();
+
         osm = Osm.getInstance();
         data = Data.getInstance();
         try {
             FileReader ler = new FileReader(file);
             XStream xstream = new XStream(new DomDriver());
-            Class<?>[] classes = new Class[] { Bounds.class, Member.class, Nd.class, Node.class, Osm.class,Relation.class,Tag.class,Way.class };
+            Class<?>[] classes = new Class[]{Bounds.class, Member.class, Nd.class, Node.class, Osm.class, Relation.class, Tag.class, Way.class};
             xstream.setupDefaultSecurity(xstream);
             xstream.allowTypes(classes);
             xstream.autodetectAnnotations(true);
-            
+
             xstream.alias("osm", Osm.class);
-            
+
             osm = (Osm) xstream.fromXML(ler);
-            
+
 //            Teste para verificar se salvou no Objeto osm 
             System.out.println("Bounds.maxlat: " + osm.getBounds().getMaxlat());
             System.out.println("Bounds.minlat: " + osm.getBounds().getMinlat());
-            
+
             System.out.println("Nodes");
-            
-                
+
+            data.setNodes(new Nodes());
+            data.setEdges(new Edges());
+            data.setConnections(new Connections());
+            data.setTypes(new Types());
+
+            data = converterES.converterES(osm, data);
+            xml = xstream.toXML(data);
+            System.out.println(xml);
             
         } catch (FileNotFoundException ex) {
             Logger.getLogger(GeradorXML.class.getName()).log(Level.SEVERE, null, ex);
         }
-        data.setNodes(new Nodes());
-        data.setEdges(new Edges());
-        data.setConnections(new Connections());
-        data.setTypes(new Types());
-        
-        data= converterES.converterES(osm,data);
+
     }
-    
-    public void geraXmlMacro(){
-        modulo2.model.saida.Node node =  new modulo2.model.saida.Node();
+
+    public void geraXmlMacro() {
+        modulo2.model.saida.Node node = new modulo2.model.saida.Node();
         node.setId("1");
-        
+
         Nodes nodes = new Nodes();
         nodes.addNode(node);
-        
+
         String msg = node.getId();
         System.out.println(msg);
-        
+
         XStream stream = new XStream(new DomDriver());
         stream.alias("name", Node.class);
-        
+
         String xml = stream.toXML(node);
-        
+
         System.out.println("<?xml versio=\"1.0\"?>");
         System.out.println(xml);
-        
-        
+
     }
-    public void geraXmlMicro(){
+
+    public void geraXmlMicro() {
         JOptionPane.showMessageDialog(null, "XML Micro");
     }
-    
+
 }
